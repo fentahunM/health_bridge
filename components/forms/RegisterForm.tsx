36eb9b93e50910b07c7c00cfb212eae094980d71
fentahunM <fantahun360@gmail.com>
2024-08-07 16:49:37 -0400

    add select and input field for identificatiion section

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -14,7 +14,7 @@ import { createUser } from "@/lib/actions/patient.actions";
 import { useRouter } from "next/navigation";
 import { FormFieldType } from "./PatientForm";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
-import { Doctors, GenderOptions } from "@/constants";
+import { Doctors, GenderOptions, IdentificationTypes } from "@/constants";
 import { Label } from "../ui/label";
 import { SelectItem } from "../ui/select";
 import Image from "next/image";
@@ -243,6 +243,34 @@ const RegisterForm = ({ user }: { user: User }) => {
             placeholder="Appendectomy, Tonsillectomy, etc."
           />
         </div>
+
+        <section className="space-y-6">
+          <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Identification and Verification</h2>
+          </div>
+        </section>
+
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.SELECT}
+          name="identificationType"
+          label="Identification Type"
+          placeholder="Select an identification type"
+        >
+          {IdentificationTypes.map((type) => (
+            <SelectItem key={type} value={type}>
+              {type}
+            </SelectItem>
+          ))}
+        </CustomFormField>
+
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.INPUT}
+          name="identificationNumber"
+          label="Identification number"
+          placeholder="013458235"
+        />
 
         <SubmitButton isLoading={isLoading}>Get started </SubmitButton>
       </form>
