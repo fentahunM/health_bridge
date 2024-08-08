967576f28728ca8ad9de7f2569552614844dc3e3
fentahunM <fantahun360@gmail.com>
2024-08-08 23:03:19 +0300

    register patient to db

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,6 +1,22 @@
 import { ID, Query } from "node-appwrite";
-import { users } from "@/lib/appwrite.config";
+import {
+  BUCKET_ID,
+  database,
+  DATABASE_ID,
+  ENDPOINT,
+  PATIENT_COLLECTION_ID,
+  PROJECT_ID,
+  storage,
+  users,
+} from "@/lib/appwrite.config";
 import { parseStringify } from "@/lib/utils";
+// import { InputFile } from "node-appwrite/file";
+
+let InputFile: any;
+if (typeof window === "undefined") {
+  // Dynamically import the 'InputFile' class only on the server side
+  InputFile = require("node-appwrite/file").InputFile;
+}
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -21,10 +37,45 @@ export const createUser = async (user: CreateUserParams) => {
   }
 };
 
-export const getUser = async (userId:string)=>{
- try{ const user = await users.get(userId);
-  return parseStringify(user);
- }catch(error){
-   console.log(error);
- }
-}
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
+    return parseStringify(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const registerPatient = async ({
+  identificationDocument,
+  ...patient
+}: RegisterUserParams) => {
+  try {
+    let file;
+    if (identificationDocument) {
+      const inputFile = InputFile.fromBuffer(
+        identificationDocument?.get("blobFile") as Blob,
+        identificationDocument?.get("fileName") as string
+      );
+      file = await storage.createFile(
+        "66b0b3fe0001cdc1616f",
+        ID.unique(),
+        inputFile
+      );
+    }
+
+    const newPatient = await database.createDocument(
+      "66b0ad370037a2156f46",
+      "66b0ad51001030e126ed",
+      ID.unique(),
+      {
+        identificationDocumentId: file?.$id || null,
+        identificationDocumentUrl: `https://cloud.appwrite.io/v1/storage/buckets/66b0b3fe0001cdc1616f/files/${file?.$id}/view?project=${PROJECT_ID}`,
+        ...patient,
+      }
+    );
+    return parseStringify(newPatient);
+  } catch (error) {
+    console.log(error);
+  }
+};
