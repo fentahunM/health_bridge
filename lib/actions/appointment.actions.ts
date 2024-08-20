f96963f07ed3eba9772c8e157e19ce84890084cd
fentahunM <fantahun360@gmail.com>
2024-08-20 22:53:02 +0300

    update appointment endpoint

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -7,6 +7,7 @@ import {
 } from "../appwrite.config";
 import { parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite.types";
+import { revalidatePath } from "next/cache";
 
 export const createAppointent = async (
   appointment: CreateAppointmentParams
@@ -72,5 +73,30 @@ export const getRecentAppointmentList = async () => {
     return parseStringify(data);
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const updateAppointment = async ({
+  appointmentId,
+  userId,
+  appointment,
+  type,
+}: UpdateAppointmentParams) => {
+  try {
+    const updatedAppointment = await databases.updateDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId,
+      appointment
+    );
+
+    if (!updatedAppointment) throw Error("Appointment nof found!");
+
+    //TODO SMS notification
+
+    revalidatePath("/admin");
+    return parseStringify(updatedAppointment);
+  } catch (error) {
+    console.error("An error occurred while scheduling an appointment:", error);
   }
 };
